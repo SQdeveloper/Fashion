4f2a880de7bf7ff868ebc1aa80ff8a066ce5acda
Jefferson <jefferson.silvaquinto@gmail.com>
2022-11-23 23:53:30 -0500

    optimizando imagenes, agregando seccion others

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
 const img = document.querySelectorAll(".scenery__contImg-img");
 const imgReduced1 = document.querySelectorAll(".scenery__img-reduced-1");
 const imgReduced2 = document.querySelectorAll(".scenery__img-reduced-2");
+const sceneryImg = document.querySelectorAll(".scenery__contImg-img"); 
+const cursor = document.querySelector(".cursor");
+const animalsImg = document.querySelectorAll(".animals__img");
+const othersImg = document.querySelectorAll(".others__contImg");
 
+
+othersImg.forEach(img =>{
+    img.addEventListener("mousemove", (event)=>{
+        cursor.style.setProperty("--x", `${event.clientX}px`);
+        cursor.style.setProperty("--y", `${event.clientY}px`);
+        cursor.style.opacity = "1"
+    });
+
+    img.addEventListener("mouseout", ()=>{
+        cursor.style.opacity = "0";
+    });
+});
+
+animalsImg.forEach(img =>{
+    img.addEventListener("mousemove", (event)=>{
+        cursor.style.setProperty("--x", `${event.clientX}px`);
+        cursor.style.setProperty("--y", `${event.clientY}px`);
+        cursor.style.opacity = "1"
+    });
+
+    img.addEventListener("mouseout", ()=>{
+        cursor.style.opacity = "0";
+    });
+});
+
+sceneryImg.forEach(scener=>{
+    scener.addEventListener("mousemove", (event)=>{
+        cursor.style.setProperty("--x", `${event.clientX}px`);
+        cursor.style.setProperty("--y", `${event.clientY}px`);
+        cursor.style.opacity = "1"
+    });
+
+    scener.addEventListener("mouseout", ()=>{
+        cursor.style.opacity = "0";
+    });
+});
+
+
+
+//efecto grayscale en las imagenes de la sección scenery
 function callback(entries) {
     entries.forEach((entry)=>{
         if(entry.isIntersecting == true){
